fde05521e53375e5dc8d4df6ff0119a215031cca
Manuel <anonymous@mail.com>
2019-05-21 17:03:31 +0200

    ...

--- src/10_particle_filter/src/ParticleFilter.cpp
+++ src/10_particle_filter/src/ParticleFilter.cpp
@@ -95,9 +95,9 @@ void ParticleFilter::integrateMotion(std::vector<Particle>& particles, const dou
  */
 double ParticleFilter::getDistanceToNearestLight(const double& x) {
 	double dist;
-	dist = abs(x - 2.0);
-	dist = dist < abs(x - 6.0) ? dist : abs(x - 6.0);
-	dist = dist < abs(x - 8.0) ? dist : abs(x - 8.0);
+
+	dist = fmin(fmin(abs(x - 2.0), abs(x - 6.0)), abs(x - 8.0));
+
 	//TODO Return the distance from the robot's position x to the nearest light source.
 	return dist;
 }
